595d7bc202eab557641ec0b39a06d1d1f0d27197
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-12-16 05:57:05 +0900

    add ball struct

--- RollingBalls.cpp
+++ RollingBalls.cpp
@@ -18,6 +18,7 @@ using namespace std;
 
 typedef long long ll;
 
+const int UNKNOWN = -1;
 const int WALL = 10;
 const int EMPTY = 11;
 
@@ -26,15 +27,52 @@ int g_height;
 // 横幅
 int g_width;
 
+// ボールの情報
+struct BALL {
+  int id;
+  int y;
+  int x;
+  int color;
+
+  BALL(int id = UNKNOWN, int y = UNKNOWN, int x = UNKNOWN, int color = UNKNOWN){
+    this->id = id;
+    this->y = y;
+    this->x = x;
+    this->color = color;
+  }
+};
+
 // 迷路
 vector< vector<int> > g_maze;
 
 // 目標
 vector< vector<int> > g_target;
 
+// ボールのリスト
+vector<BALL> g_ball_list;
+
 inline int char2int(char ch){
   return ch - '0';
 }
+
+string int2string(int number){
+  stringstream ss; 
+  ss << number;
+  return ss.str();
+}
+
+unsigned long long xor128(){
+  static unsigned long long rx=123456789, ry=362436069, rz=521288629, rw=88675123;
+  unsigned long long rt = (rx ^ (rx<<11));
+  rx=ry; ry=rz; rz=rw;
+  return (rw=(rw^(rw>>19))^(rt^(rt>>8)));
+}
+
+struct NODE {
+  string operation; // ボールの操作
+  NODE *parent;     // 親のノード
+  ll hash;          // 盤面のハッシュ値
+};
 
 class RollingBalls {
   public:
@@ -46,6 +84,9 @@ class RollingBalls {
       init_target(target);
     }
 
+    /**
+     * mazeの初期化
+     */
     void init_maze(vector<string> start){
       g_maze = vector< vector<int> >(g_height, vector<int>(g_width));
 
@@ -60,11 +101,24 @@ class RollingBalls {
           }else{
             int color = char2int(ch);
             g_maze[y][x] = color;
+            g_ball_list.push_back(create_ball(y, x, color));
           }
         }
       }
     }
 
+    /**
+     * ボールの作成
+     */
+    BALL create_ball(int y, int x, int color){
+      int id = g_ball_list.size();
+
+      return BALL(id, y, x, color);
+    }
+
+    /**
+     * targetフィールドの初期化
+     */
     void init_target(vector<string> target){
       g_target = vector< vector<int> >(g_height, vector<int>(g_width));
 
@@ -84,6 +138,9 @@ class RollingBalls {
       }
     }
 
+    /**
+     * ソルバー
+     */
     vector<string> restorePattern(vector<string> start, vector<string> target){
       vector<string> result;
 
@@ -91,28 +148,36 @@ class RollingBalls {
 
       return result;
     }
+
+    /**
+     * ボールを転がす
+     */
+    void roll_ball(int y, int x, int direct){
+    }
+
+    /**
+     * ボールの操作コマンドを生成
+     */
+    string create_query(int y, int x, int direct){
+      string query = "";
+      query += int2string(y) + " ";
+      query += int2string(x) + " ";
+      query += int2string(direct);
+
+      return query;
+    }
 };
 
 int main(){
   int h;
   string str;
-  vector<string> start;
-  vector<string> target;
+  vector<string> start, target;
   cin >> h;
-
-  for(int i = 0; i < h; i++){
-    cin >> str;
-    start.push_back(str);
-  }
-  for(int i = 0; i < h; i++){
-    cin >> str;
-    target.push_back(str);
-  }
+  for(int i=0;i<h;i++){cin >> str;start.push_back(str);}
+  for(int i=0;i<h;i++){cin >> str;target.push_back(str);}
   RollingBalls rb;
   vector<string> ret = rb.restorePattern(start, target);
   cout << ret.size() << endl;
-  for(int i = 0; i < ret.size(); i++){
-    cout << ret[i] << endl;
-  }
+  for(int i=0;i<ret.size();i++){cout << ret[i] << endl;}
   return 0;
 }
